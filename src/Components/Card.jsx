b6ad5df0da9971748b6c22fb0e6b0bce4d3ef975
Rohit Mourya <rohitmourya0012@gmail.com>
2022-09-08 18:09:04 +0530

    check

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,10 @@
 import {Box,Text,Image} from "@chakra-ui/react"
+import {Link} from "@chakra-ui/react"
 export default function Card({avatar,title,desc}){
     return(<>
     <Box className="card">
         <Image src={avatar} alt="..." height="230px"></Image>
-        <Text className="title">{title}</Text>
+        <Link to="/"><Text className="title">{title}</Text></Link>
         <Text className="desc">{desc}</Text>
     </Box>
     </>)
